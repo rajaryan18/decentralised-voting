5dc5efb3152aedb740ee48af91401865dcf1d6ac
legendary0911 <85240150+legendary0911@users.noreply.github.com>
2022-12-23 23:10:54 +0530

    variable name changed

--- web3/contracts/Election.sol
+++ web3/contracts/Election.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract Election {
+contract ElectionInfo {
     //@VARIABLES
     // holds data of a particular election
     // current stage of the election
@@ -10,7 +10,7 @@ contract Election {
         ONGOING,
         END
     }
-    struct Voting {
+    struct Election {
         string name;
         uint256 start_date;
         uint256 end_date;
@@ -21,27 +21,21 @@ contract Election {
         address ownerAddress;
     }
 
-    
-
-    mapping(uint256 => Voting) public votings;
+    mapping(uint256 => Election) public votings;
     uint256 public numberOfVoting = 0;
-
-    
 
     //@MODIFIERS
     // only owner modifier
-    modifier onlyOwner(uint _id) {
-        require(msg.sender == votings[_id].ownerAddress);
+    modifier onlyOwner(uint256 _electionid) {
+        require(msg.sender == votings[_electionid].ownerAddress);
         _;
     }
 
     //state check modifier
-    modifier inPhase(Phase _phase, uint256 _id) {
-        require(votings[_id].currPhase == _phase);
+    modifier inPhase(Phase _phase, uint256 _electionid) {
+        require(votings[_electionid].currPhase == _phase);
         _;
     }
-
-
 
     //@FUNCTIONS
 
@@ -51,12 +45,13 @@ contract Election {
     }
 
     // to enable upgrades we need to set up Proxies hence no constructors
+    //both start and end date should be in unix format in seconds elapsed
     function init(
         string memory _name,
         uint256 _start_date,
         uint256 _end_date
     ) public returns (uint256) {
-        Voting storage voting = votings[numberOfVoting];
+        Election storage voting = votings[numberOfVoting];
         //checks if everything is okay
         require(
             _start_date > block.timestamp,
@@ -78,26 +73,33 @@ contract Election {
     }
 
     //to add Candidates
-    function addCandidates(string memory _name, uint256 _id)
+    function addCandidates(string memory _name, uint256 _electionid)
         public
-        onlyOwner(_id)
-        inPhase(Phase.PRESTART, _id)
+        onlyOwner(_electionid)
+        inPhase(Phase.PRESTART, _electionid)
     {
-        votings[_id].candidates.push(_name);
+        votings[_electionid].candidates.push(_name);
     }
 
     //function to get all candidates of particular voting id
-    function getCandidates(uint256 _id)
+    function getCandidates(uint256 _electionid)
         public
         view
         returns (string[] memory, uint256[] memory)
     {
-        return (votings[_id].candidates, votings[_id].numberOfVotes);
+        return (
+            votings[_electionid].candidates,
+            votings[_electionid].numberOfVotes
+        );
     }
 
     //function to start voting, only official can start voting (here phase changes from PRESTART to ONGOING)
-    function startVoting(uint256 _id) public onlyOwner(_id) inPhase(Phase.PRESTART, _id) {
-        votings[_id].currPhase = Phase.ONGOING;
+    function startVoting(uint256 _electionid)
+        public
+        onlyOwner(_electionid)
+        inPhase(Phase.PRESTART, _electionid)
+    {
+        votings[_electionid].currPhase = Phase.ONGOING;
     }
 
     //function to do vote
@@ -105,25 +107,29 @@ contract Election {
     //_aadharHash is the person who has voted and we are storing it in voted[]
     //function returns array of number of votes given to each candidate
     function doVote(
-        uint256 _id,
+        uint256 _electionid,
         uint256 _candidateID,
         string memory _aadhar
-    ) public inPhase(Phase.ONGOING, _id) returns (uint256[] memory) {
-        for (uint256 i = 0; i < votings[_id].voted.length; i++) {
+    ) public inPhase(Phase.ONGOING, _electionid) returns (uint256[] memory) {
+        for (uint256 i = 0; i < votings[_electionid].voted.length; i++) {
             //check if user has already voted
             require(
-                keccak256(abi.encodePacked(votings[_id].voted[i])) ==
+                keccak256(abi.encodePacked(votings[_electionid].voted[i])) ==
                     keccak256(abi.encodePacked(_aadhar)),
                 "Voter has already voted"
             );
         }
-        votings[_id].numberOfVotes[_candidateID]++;
-        votings[_id].voted.push(hashAadhar(_aadhar));
-        return votings[_id].numberOfVotes;
+        votings[_electionid].numberOfVotes[_candidateID]++;
+        votings[_electionid].voted.push(hashAadhar(_aadhar));
+        return votings[_electionid].numberOfVotes;
     }
 
     //function to end voting
-    function endVoting(uint256 _id) public onlyOwner(_id) inPhase(Phase.ONGOING, _id) {
-        votings[_id].currPhase = Phase.END;
+    function endVoting(uint256 _electionid)
+        public
+        onlyOwner(_electionid)
+        inPhase(Phase.ONGOING, _electionid)
+    {
+        votings[_electionid].currPhase = Phase.END;
     }
 }
