817131e9f3db1aac4d795e5bfbe1ab2f4bd6666b
Prateek Krishna <prateek@Prateek-MBA.local>
2022-12-23 20:17:40 +0530

    updated election.sol

--- web3/contracts/Election.sol
+++ web3/contracts/Election.sol
@@ -5,50 +5,94 @@ contract Election {
 
     //@VARIABLES
     // holds data of a particular election
-    uint256 public id;
-    string public name;
-    address public ownerAddress;
-    string public start_date;
-    string public end_date;
-    string[] public verifications;
-    string[] public candidates;
+    struct Voting {
+        uint256 id;
+        string name;
+        uint256 start_date;
+        uint256 end_date;
+        string[] verifications;
+        string[] candidates;
+        uint256[] numberOfVotes;
+        string[] voted;
+    }
+    
     // current stage of the election
     enum Phase { PRESTART, ONGOING, END }
     Phase public currPhase;
-    // candidate id (index of the candidates array) mapped to the number of votes
-    mapping(uint256 => uint256) public votes;
+
+    mapping(uint256 => Voting) public votings;
+    uint256 public numberOfVoting = 0;
+
+    address ownerAddress;
 
 
     //@MODIFIERS
-    //only owner modifier
-    
+    // only owner modifier
     modifier onlyOwner(){
         require(msg.sender == ownerAddress);
+        _;
+    }
+
+    //state check modifier
+    modifier inPhase(Phase _phase){
+        require(currPhase == _phase);
         _;
     }
 
 
     //@FUNCTIONS
     // to enable upgrades we need to set up Proxies hence no constructors
-    function init(uint256 _id, string memory _name, string memory _start_date, string memory _end_date, string[] memory _verifications, string[] memory _candidates) external {
-        id = _id;
-        name = _name;
+    function init(uint256 _id, string memory _name, uint256 _start_date, uint256 _end_date, string[] memory _verifications, string[] memory _candidates) public returns(uint256) {
+        Voting storage voting = votings[numberOfVoting];
+        //checks if everything is okay
+        require(voting.end_date < block.timestamp, "The deadline should be date in future");
+        
+        voting.id = _id;
+        voting.name = _name;
+        voting.start_date = _start_date;
+        voting.end_date = _end_date;
+        voting.verifications = _verifications;
+        voting.candidates = _candidates;
+
         ownerAddress = msg.sender;
-        start_date = _start_date;
-        end_date = _end_date;
-        verifications = _verifications;
-        candidates = _candidates;
         currPhase = Phase.PRESTART;
+        numberOfVoting++;
+
+        return numberOfVoting;
     }
 
-    // to add new candidates
-    function addCandidates(string memory _name) external returns(uint256) {
-        // No changes once Election begins
+    
+    //to add Candidates
+    function addCandidates(string memory _name, uint256 _id) public onlyOwner inPhase(Phase.PRESTART) {
         require(currPhase == Phase.PRESTART);
-        candidates.push(_name);
-        // id is the index starting from 1
-        return candidates.length;
+        votings[_id].candidates.push(_name);
     }
 
+    //function to get all candidates of particular voting id
+    function getCandidates(uint256 _id) view public returns(string[] memory, uint256[] memory) {
+        return (votings[_id].candidates, votings[_id].numberOfVotes);
+    }
+
+    //function to start voting, only official can start voting (here phase changes from PRESTART to ONGOING)
+    function startVoting() public onlyOwner inPhase(Phase.PRESTART) {
+        currPhase = Phase.ONGOING;
+    }
+
+    //function to do vote
+    //here _id is id of voting, and _name is name of candidate to whom vote is given. 
+    //_aadharHash is the person who has voted and we are storing it in voted[]
+    //function returns array of number of votes given to each candidate
+    function doVote(uint256 _id, uint256 _candidateID, string memory _aadharHash) public inPhase(Phase.ONGOING) returns(uint256[] memory){
+        for(uint256 i=0; i<votings[_id].voted.length; i++){
+            //check if user has already voted
+            require(keccak256(abi.encodePacked(votings[_id].voted[i])) == keccak256(abi.encodePacked(_aadharHash)), "Voter has already voted");
+        }
+        votings[_id].numberOfVotes[_candidateID]++;
+        return votings[_id].numberOfVotes;
+    }
+
+    function endVoting() public onlyOwner inPhase(Phase.ONGOING){
+        currPhase = Phase.END;
+    }
     
 }
