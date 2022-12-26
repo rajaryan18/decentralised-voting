a3645c8965cd02b643e662d3890437fa13093e97
Prateek Krishna <prateek@Prateek-MBA.local>
2022-12-26 22:19:35 +0530

    bug fix

--- client/components/CandidateCard.jsx
+++ client/components/CandidateCard.jsx
@@ -5,8 +5,8 @@ import trump from '../public/trump.jpg'
 const CandidateCard = () => {
   return (
     <div className='h-[120px] w-[97%] bg-yellow-100 rounded-[15px]' >
-        <div className='flex justify-center'>
-        <Image src={trump} alt="vote" className="w-[50px] h-[50px] rounded-full"/>
+        <div className='flex justify-start' >
+        <Image src={trump} alt="vote" className="w-[50px] h-[50px] rounded-full self-center"/>
         </div>
     </div>
   )
