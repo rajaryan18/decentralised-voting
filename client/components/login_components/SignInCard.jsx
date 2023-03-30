220465649cd5afaf62c45b534a996944ce63cfef
legendary0911 <85240150+legendary0911@users.noreply.github.com>
2023-03-30 15:43:12 +0530

    metamask butoon added

--- client/components/login_components/SignInCard.jsx
+++ client/components/login_components/SignInCard.jsx
@@ -2,27 +2,43 @@
 import Image from 'next/image'
 import meta from "../../public/metamask.png"
 import ethe from "../../public/trio.png"
+import { useStateContext } from '../../context';
+import { useDisconnect } from '@thirdweb-dev/react';
+import { useRouter } from 'next/router';
 
-const SignInCard = (props) => (
-    <div className='di relative sm:h-[300px] sm:w-[500px] h-[300px] w-[300px] mb-20 overflow-hidden rounded-xl bg-[#111526] mt-12 '>
-        <div className="absolute z-20 inset-[1.5px] rounded-[12px] ">
-            <div className="bg-[#283457] text-white px-2 py-4  rounded-t-xl h-[130px]">
-                <div className='contenttext-4xl text-center text-2xl md:text-3xl  text-white'>Welcome back to De<span className='text-[#60e0e6]'>ction</span></div>
-                <div className=' flex justify-center'><Image src={ethe} className="h-28 w-28 -mt-4 mx-auto" /></div>
+
+const SignInCard = (props) => {
+    const { connect, address } = useStateContext();
+    const disconnect = useDisconnect();
+    const router = useRouter();
+    const handleSignIn = () => {
+        if (address) router.push('/dashboard')
+        else alert("Wallet not connected")
+    }
+
+    return (
+
+        <div className='di relative sm:h-[300px] sm:w-[500px] h-[300px] w-[300px] mb-20 overflow-hidden rounded-xl bg-[#111526] mt-12 '>
+            <div className="absolute z-20 inset-[1.5px] rounded-[12px] ">
+                <div className="bg-[#283457] text-white px-2 py-4  rounded-t-xl h-[130px]">
+                    <div className='contenttext-4xl text-center text-2xl md:text-3xl  text-white'>Welcome back to De<span className='text-[#60e0e6]'>ction</span></div>
+                    <div className=' flex justify-center'><Image src={ethe} className="h-28 w-28 -mt-4 mx-auto" /></div>
+                </div>
+                <button onClick={address ? () => { disconnect() } : () => { connect() }} className=" bg-gradient-to-r flex from-orange-600 to cursor-pointer hover:scale-105 duration-200 hover:shadow-lg shadow-black  bg-orange-800 h-[50px] justify-center text-white rounded-xl mt-3 mx-auto w-[80%] text-center py-3">
+                    {address ? "Connected" : "Connect metamask"}<Image src={meta} className="h-6 w-6 mt-[1px] ml-2" />
+
+                </button>
+                <button onClick={handleSignIn} className=" bg-gradient-to-r flex from-[#60e0e6] to-[#03b7c1] cursor-pointer hover:scale-105 duration-200 hover:shadow-lg shadow-black  bg-[] h-[50px] w-[80%] justify-center text-white rounded-xl mt-3 mx-auto text-center py-3">
+                    Sign In
+
+                </button>
+                <p className='text-white text-sm text-center mt-4'>New to Dection? <a onClick={props.on} className=' underline hover:text-blue-300 cursor-pointer'>Sign up</a></p>
             </div>
-            <a className=" bg-gradient-to-r flex from-orange-600 to cursor-pointer hover:scale-105 duration-200 hover:shadow-lg shadow-black  bg-orange-800 h-[50px] justify-center text-white rounded-xl mt-3 mx-auto w-[80%] text-center py-3">
-                Connect Metamask<Image src={meta} className="h-6 w-6 mt-[1px] ml-2" />
-
-            </a>
-            <a className=" bg-gradient-to-r flex from-[#60e0e6] to-[#03b7c1] cursor-pointer hover:scale-105 duration-200 hover:shadow-lg shadow-black  bg-[] h-[50px] w-[80%] justify-center text-white rounded-xl mt-3 mx-auto text-center py-3">
-                Sign In
-
-            </a>
-            <p className='text-white text-sm text-center mt-4'>New to Dection? <a onClick={props.on} className=' underline hover:text-blue-300 cursor-pointer'>Sign up</a></p>
         </div>
-    </div>
 
-);
+    );
+}
+
 
 export default SignInCard;
 
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,15 @@
 import '../styles/globals.css'
+import { ThirdwebProvider } from '@thirdweb-dev/react'
+import { StateContextProvider } from '../context'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider >
+
+      <StateContextProvider>
+        <Component {...pageProps} />
+      </StateContextProvider>
+
+    </ThirdwebProvider>
+  );
 }
