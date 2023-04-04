1aaac0fd22de23623dfea0e2649bec8d937d866e
legendary0911 <85240150+legendary0911@users.noreply.github.com>
2023-04-04 14:28:04 +0530

    profile page added

---
+++ client/components/profileElectionCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image from 'next/image'
+import person from '../public/person.jpg'
+
+const ProfileElectionCard = (props) => {
+    return (
+        <div className='bg-white rounded-md h-36 w-40' >
+
+        </div>
+    )
+}
+
+export default ProfileElectionCard
