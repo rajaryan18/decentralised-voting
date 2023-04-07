39b89e495f29498dfe1377fb886d575ddda037ed
Raj Aryan <73771884+rajaryan18@users.noreply.github.com>
2023-04-07 23:37:24 +0530

    Smart Contract updated

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -2,8 +2,9 @@
 require('@openzeppelin/hardhat-upgrades');
 require("@nomicfoundation/hardhat-chai-matchers");
 
-const ALCHEMY_API_KEY = "H3s3yj_XRFzXqvqt8vxzxinZyC4d30nX";
-const GOERLI_PRIVATE_KEY = "92262f3915078dc99bb6a4b7ddebdd624b4691a70de901c9e3c6f2fbe0d5fd26";
+const ALCHEMY_API_KEY = "C80v5nFaMkwABAnonDP7UaVAcfIvtmzW";
+const PRIVATE_KEY = "0x92262f3915078dc99bb6a4b7ddebdd624b4691a70de901c9e3c6f2fbe0d5fd26";
+
 
 module.exports = {
   solidity: {
@@ -18,7 +19,11 @@ module.exports = {
   networks: {
     goerli: {
       url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [GOERLI_PRIVATE_KEY]
+      accounts: [PRIVATE_KEY]
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY]
     }
   }
 };
