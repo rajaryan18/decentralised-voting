7349c317b4a2f1e60b93ca998e03f29290bf9162
legendary0911 <85240150+legendary0911@users.noreply.github.com>
2023-04-07 23:55:25 +0530

    profile page updated

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["www.google.com"],
+    formats: ["image/webp"],
+  },
 }
 
 module.exports = nextConfig
