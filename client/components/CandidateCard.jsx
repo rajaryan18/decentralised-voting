bb30c976cb2eaf8d0f89b3445ed2985b13c7822d
legendary0911 <85240150+legendary0911@users.noreply.github.com>
2023-04-12 13:06:57 +0530

    candidates page made responsive

--- client/components/CandidateCard.jsx
+++ client/components/CandidateCard.jsx
@@ -3,12 +3,12 @@ import Image from 'next/image'
 import person from '../public/person.jpg'
 import { useStateContext } from "../context";
 
-const CandidateCard = ({name, party, electionId, candidateId}) => {
+const CandidateCard = ({ name, party, electionId, candidateId }) => {
 
-  const {doVote, userinfo} = useStateContext()
+  const { doVote, userinfo } = useStateContext()
 
   const aadhar_num = userinfo.aadhar;
-  const handleVote = () =>{
+  const handleVote = () => {
     try {
       doVote(electionId, candidateId, aadhar_num)
     } catch (error) {
@@ -16,17 +16,17 @@ const CandidateCard = ({name, party, electionId, candidateId}) => {
     }
   }
   return (
-    <div className='h-[120px] w-[97%] bg-[#1e2742] rounded-tr-[15px] rounded-br-[15px] rounded-tl-[40px] rounded-bl-[40px] overflow-visible ' >
-        <div className='flex items-center'>
-        <Image src={person} alt="vote" className="w-[130px] h-[130px] rounded-full ml-[-10px] mt-[-5px]"/>
-        <div className=" ml-[5px] h-[120px] w-[50%] mt-[-5px] rounded-[15px] flex flex-col justify-start p-[15px] gap-[20px] self-center" >
-            <p className='text-white' ><span className='text-gradient' >Name: </span> {name}</p>
-            <p className='text-white' ><span className='text-gradient' >Party: </span> {party}</p>
+    <div className='h-fit w-[100%] bg-[#1e2742] rounded-b-md rounded-t-3xl md:rounded-tr-[15px] md:rounded-br-[15px] md:rounded-tl-[40px] md:rounded-bl-[40px] overflow-visible ' >
+      <div className='flex md:flex-row flex-col items-center'>
+        <Image src={person} alt="vote" className="w-[130px] h-[130px] rounded-full ml-[-10px] md:mt-[-5px] -mt-3" />
+        <div className=" ml-[5px] h-[120px] w-[90%] md:w-[80%] mt-[-5px] rounded-[15px] flex flex-col justify-center text-center md:text-start md:justify-start p-[5px] gap-2 md:gap-[20px] self-center" >
+          <p className='text-white' ><span className='text-gradient' >Name: </span> {name}</p>
+          <p className='text-white' ><span className='text-gradient' >Party: </span> {party}</p>
         </div>
-        <div onClick={handleVote} className="h-[40px] w-[120px] bg-gradient-to-r from-orange-500 via-golden-500 to-yellow-500 rounded-[15px] flex justify-center items-center font-epilogue hover:cursor-pointer hover:scale-105 hover:shadow-md duration-300" >
-            <p>Cast Vote</p>
+        <div onClick={handleVote} className="h-[40px] w-[120px] mb-4 mr-0 md:mr-4 md:mb-0 bg-gradient-to-r from-orange-500 via-golden-500 to-yellow-500 rounded-[15px] flex justify-center items-center font-epilogue hover:cursor-pointer hover:scale-105 hover:shadow-md duration-300" >
+          <p>Cast Vote</p>
         </div>
-        </div>
+      </div>
     </div>
   )
 }
--- client/pages/election/[id].js
+++ client/pages/election/[id].js
@@ -7,67 +7,72 @@ import { OrbitControls } from "@react-three/drei";
 import { Model1 } from "../../components/threejs/Echain";
 import { useStateContext } from "../../context";
 import { useEffect, useState } from "react";
+import { electionData } from "../../components/constants";
 
 
 const id = () => {
   const router = useRouter();
   const electionId = router.query.id;
   const { getElectionResults, getElectionOfUser, getElectionById, startVoting } = useStateContext();
+  const [currentphase, setCurrentphase] = useState(1);
 
-  const [election, setElection] = useState({table: []})
+  const [election, setElection] = useState({ table: [] })
 
-  useEffect(()=>{
+  useEffect(() => {
     try {
-      getElectionById(electionId).then((data)=>{
-        setElection({...election, table: data})
+      getElectionById(electionId).then((data) => {
+        setElection({ ...election, table: data })
+
       })
     } catch (error) {
       console.log(error);
     }
   }, [])
 
+  if (currentphase === 0) {
+    return (
+      <div className="flex text-white items-center bg-primary bg-[#01040f] justify-center text-center h-[1000px]">pre voting phase</div>
+    );
 
-  return (
-    <div className="bg-primary bg-[#01040f] w-full overflow-hidden flex flex-col align-center pb-[100px]">
-      {console.log(election?.table?.candidates)}
-      <div className="px-[50px] mt-[10px] w-[600px] flex">
-        <div className="flex flex-col">
+  } else if (currentphase === 1) {
+    return (
+      <div className="bg-primary bg-[#01040f] w-full overflow-hidden flex flex-col align-center pb-[100px]">
+        {console.log(election?.table?.candidates)}
+        {/* {console.log("phase is" + election?.table?.candidates)} */}
+        <div className="mx-auto h-fit flex-col w-[85%] mt-[10px] rounded-[20px] flex pl-0 md:pl-2 md:text-start text-center items-center justify-center md:items-start md:justify-start   overflow-visible">
           <h2 className="font-bold text-white text-[40px] w-[600px]">
-            Hello Prateek,
+            Hello <br className="md:hidden block" /> Prateek,
           </h2>
-          <p className="text-gradient font-bold text-[20px] w-[600px]">
-            Welome to decentralized Election
+          <p className="text-gradient first-letter:font-bold text-[20px] w-[600px]">
+            Welome to <br className="md:hidden block" /> Decentralized Election
           </p>
         </div>
-      </div>
-      <div className="ml-[50px] h-[170px] w-[85%] bg-blue-gradient mt-[10px] rounded-[20px] flex  pl-[20px] overflow-visible">
-        <p className="font-epilogue font-bold sm:text-[30px] md:text-[60px] text-black mt-[25px] mb-[25px]">
-          Every Vote Counts!
-        </p>
-        <div className="absolute z-[0] w-[10%] h-[40%] top-0 pink__gradient" />
-        <div className="absolute z-[1] w-[30%] h-[70%] rounded-full white__gradient bottom-40" />
-        <div className="absolute z-[0] w-[15%] h-[45%] right-20 bottom-20 blue__gradient" />
-        <Image
-          src={vote}
-          alt="vote"
-          className="w-[290px] h-[290px] pr-[-50px] mt-[-120px] absolute  right-[12%]"
-        />
-      </div>
-      <div className="flex w-[100%]">
-        <div className=" w-[50%] ml-[50px] mt-[20px] to-transparent rounded-[20px] p-[20px] flex flex-col gap-[20px]">
-          {election?.table?.candidates?.map((candidate, ind)=>
-            <CandidateCard 
-              key={ind}
-              name = {candidate?.name}
-              party = {candidate?.party}
-              electionId = {electionId}
-              candidateId={ind}
-            />
-          )}
+        <div className="mx-auto h-[170px] w-[85%] bg-blue-gradient mt-[10px] rounded-[20px] flex  pl-0 md:pl-[20px] overflow-visible">
+          <p className="font-epilogue font-bold flex items-center justify-center text-center md:items-start md:mx-0 mx-auto md:text-start md:justify-start text-4xl md:text-5xl lg:text-6xl text-black mt-[25px] mb-[25px]">
+            Every Vote <br className="md:hidden block" />Counts!
+          </p>
+
+          <Image
+            src={vote}
+            alt="vote"
+            className="hidden lg:block w-[290px] h-[290px] pr-[-50px] mt-[-120px] absolute  right-[12%]"
+          />
         </div>
-        <div className=" w-[50%] mr-[50px] mt-[20px] to-transparent rounded-[20px] p-[20px] flex justify-center items-center">
-          <div className="h-[500px] w-[400px] ">
-            <Canvas className="mt-8">
+        <div className="flex flex-col lg:flex-row w-[100%]">
+          <div className=" w-[90%] md:w-[75%] mx-auto mt-[20px] to-transparent rounded-[20px] p-[20px] flex flex-col gap-[20px]">
+            {election?.table?.candidates?.map((candidate, ind) =>
+              <CandidateCard
+                key={ind}
+                name={candidate?.name}
+                party={candidate?.party}
+                electionId={electionId}
+                candidateId={ind}
+              />
+            )}
+          </div>
+          {/* <div className=" w-[50%] mr-[50px] mt-[20px] to-transparent rounded-[20px] p-[20px] flex justify-center items-center">
+          <div className="h-[500px] w-[400px] hidden  lg:block">
+            <Canvas className="mt-8 hidden lg:block">
               <OrbitControls
                 enableZoom={false}
                 autoRotate={true}
@@ -85,13 +90,19 @@ const id = () => {
               <Model1 />
             </Canvas>
           </div>
+        </div> */}
         </div>
+
       </div>
-      {/* <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
-      <div className="absolute z-[1] w-[30%] h-[50%] rounded-full white__gradient bottom-40" />
-      <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" /> */}
-    </div>
-  );
+    );
+
+  } else {
+    return (
+      <div className="text-white flex items-center bg-primary bg-[#01040f] justify-center text-center h-[1000px]">past voting phase</div>
+    );
+
+  }
+
 };
 
 export default id;
